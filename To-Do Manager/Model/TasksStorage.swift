2f52b255163cfbec2398a266cdb74f42ba761110
Sergei Smirnov <lam97@yandex.ru>
2022-08-30 12:49:49 +0300

    add test task

---
+++ To-Do Manager/Model/TasksStorage.swift
@@ -0,0 +1,27 @@
+//
+//  TasksStorage.swift
+//  To-Do Manager
+//
+//  Created by Сергей Смирнов on 30.08.2022.
+//
+
+protocol TasksStorageProtocol {
+    func loadTasks() -> [TaskProtocol]
+    func saveTasks(_ tasks: [TaskProtocol])
+}
+
+class TaskStorage: TasksStorageProtocol {
+    func loadTasks() -> [TaskProtocol] {
+        let testTasks: [TaskProtocol] = [
+            Task(title: "Byu beard", type: .normal, status: .planned),
+            Task(title: "Washing a cat", type: .important, status: .planned),
+            Task(title: "Arnold's dept", type: .important, status: .completed),
+            Task(title: "Byu vacuum cleaner", type: .normal, status: .completed),
+            Task(title: "Give flowers wife", type: .important, status: .planned),
+            Task(title: "Call to perents", type: .important, status: .planned),
+        ]
+        return testTasks
+    }
+    
+    func saveTasks(_ tasks: [TaskProtocol]) {}
+}
