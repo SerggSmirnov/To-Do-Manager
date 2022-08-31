4615eacc4853208b028b22377f622fa5c0316a43
Sergei Smirnov <lam97@yandex.ru>
2022-08-31 20:19:06 +0300

    add cell with stack view

---
+++ To-Do Manager/View/Cells/TaskCell.swift
@@ -0,0 +1,15 @@
+//
+//  TaskCell.swift
+//  To-Do Manager
+//
+//  Created by Сергей Смирнов on 31.08.2022.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    @IBOutlet var symbol: UILabel!
+    @IBOutlet var title: UILabel!
+
+}
